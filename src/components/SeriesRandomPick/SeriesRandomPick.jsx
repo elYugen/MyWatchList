04f715e53ad41a485bc1c3dbbe2221bb07e668a7
elyugen <leo.langlois123@gmail.com>
2025-07-10 18:28:46 +0200

    correction bug sur le randompick serie

--- src/components/SeriesRandomPick/SeriesRandomPick.jsx
+++ src/components/SeriesRandomPick/SeriesRandomPick.jsx
@@ -26,7 +26,7 @@ function SeriesRandomPick({ onAdd }) {
         return;
       }
 
-      fetch('http://127.0.0.1:8000/api/watchlist', {
+      fetch('https://api.watchlist.lleroy.fr/api/watchlist', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
